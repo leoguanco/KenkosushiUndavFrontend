41b0121ed102d4cd206f664a42decb3452709426
Leo Guanco <leo.guanco@gmail.com>
2017-11-17 23:06:18 -0300

    Servicio de productos

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { GLOBAL } from './global';
+
+@Injectable()
+export class ProductService {
+  private api: string;
+
+  constructor(private _http: Http) {
+    this.api = GLOBAL.api;
+  }
+
+  headers = new Headers({
+    'Content-Type': 'application/json'
+  });
+
+  addProduct(body) {
+    const json = JSON.stringify(body);
+    return this._http.post(this.api + '/admin/login', json , {headers: this.headers});
+  }
+
+  updateProduct(id, body) {
+    const json = JSON.stringify(body);
+    return this._http.put(this.api + '/admin/products/' + id, json, {headers: this.headers});
+  }
+
+  deleteProduct(id) {
+    return this._http.delete(this.api + '/admin/products/' + id, {headers: this.headers});
+  }
+
+  getAllProduct() {
+    return this._http.get(this.api + '/public/products/', {headers: this.headers});
+  }
+
+  getProduct(id) {
+    return this._http.get(this.api + '/public/products/' + id, {headers: this.headers});
+  }
+
+}
