1b371ab94530b876385c80370bd690b5d824f624
ima <imanollew@hotmail.com>
2017-11-22 23:08:34 -0300

    se modifico el componente del usuario

--- src/app/modules/components/user/user.component.ts
+++ src/app/modules/components/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../../services/user.service";
 
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -18,6 +19,15 @@ export class UserComponent implements OnInit {
   public errorMessage;
   public users;
   public user;
+
+  public address: string;
+  public floor: string;
+  public office: string;
+  public typeAdress: string;
+
+  public phone: number;
+  public typePhone: string;
+
 
 
   constructor(private userService: UserService) { }
@@ -124,6 +134,157 @@ export class UserComponent implements OnInit {
         }
       );
   }
+
+  addUserAddress(){
+    const data={
+      address: this.address,
+      floor: this.floor,
+      office: this.office,
+      typeAddress: this.typeAdress
+    };
+
+    this.userService.addUserAddress(data)
+      .subscribe(
+        response => {
+          console.log(response);
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+  }
+
+  updateUserAddress(item){
+    const data={
+      address: this.address,
+      floor: this.floor,
+      office: this.office,
+      typeAddress: this.typeAdress
+    };
+
+    this.userService.updateUserAddress(item.id, data)
+      .subscribe(
+        response => {
+          console.log(response);
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+
+
+  }
+
+  deleteUserAddress(item){
+    this.userService.deleteUserAddress(item.id)
+      .subscribe(
+        response => {
+          console.log(response);
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+  }
+
+  getAllUserAddress(){
+    this.userService.getAllUserAddress()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.users = response;
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+  }
+
+  addUserPhone(){
+    const data={
+      phone: this.phone,
+      typePhone: this.typePhone
+    };
+
+    this.userService.addUserPhone(data)
+      .subscribe(
+        response => {
+          console.log(response);
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+  }
+
+  updateUserPhone(item){
+    const data={
+      phone: this.phone,
+      typePhone: this.typePhone
+    };
+
+    this.userService.updateUserPhone(item.id, data)
+      .subscribe(
+        response => {
+          console.log(response);
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+
+  }
+
+  deleteUserPhone(item){
+    this.userService.deleteUserPhone(item.id)
+      .subscribe(
+        response => {
+          console.log(response);
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+  }
+
+  getAllUserPhone(){
+    this.userService.getAllUserPhone()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.users = response;
+        }, error => {
+          this.errorMessage = <any>error;
+
+          if (this.errorMessage !== null) {
+            console.log(this.errorMessage);
+          }
+        }
+      );
+  }
+
+
 
 
 
