ae558efa974f6be99f416da79c2a7707d92d60d5
Leo Guanco <leo.guanco@gmail.com>
2017-11-25 17:39:40 -0300

    Nuevas rutas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule} from '@angular/router';
 
 import { LoginComponent } from './modules/components/login/login.component';
 import { HomeComponent } from './modules/components/home/home.component';
+import {ConfigurationComponent} from './modules/components/configuration/configuration.component';
+import {ProductComponent} from './modules/components/product/product.component';
+import {UserComponent} from './modules/components/user/user.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
-  { path: '**', component: LoginComponent}
+  { path: 'configuration', component: ConfigurationComponent},
+  { path: 'product', component: ProductComponent},
+  { path: 'user', component: UserComponent},
+  { path: '**', component: HomeComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
