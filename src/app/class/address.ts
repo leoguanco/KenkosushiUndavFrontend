763630371eff87c45575b0a10f24af6fa9b47e52
Leo Guanco <leo.guanco@gmail.com>
2017-11-24 22:27:54 -0300

    Se completo address

---
+++ src/app/class/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+  constructor(
+    public id: number,
+    public address: string,
+    public floor: string,
+    public office: string,
+    public typeAddress: string,
+    public userId: number) { }
+}
